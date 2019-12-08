5d708063059ec95823b707883b30b4963b8b0b43
Ahmed <ahmed-mohammed.baghawitah@etu.univ-nantes.fr>
2019-12-08 20:36:05 +0100

    extraireCroissance done with a lil problem

--- projet.cpp
+++ projet.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-typedef char DATATYPE;
+typedef string DATATYPE;
 
 typedef struct _datum{
     DATATYPE valeur;
@@ -40,6 +40,26 @@ p_data ajoutFin(DATATYPE val, p_data head){
 
 }
 
+void ajoutFinV2(DATATYPE val, p_data head){
+    p_data toAdd;
+    toAdd = (data*)malloc(sizeof(data));
+    toAdd->valeur = val;
+    toAdd->suiv = nullptr;
+
+
+    p_data tmp = head;
+
+    while((tmp->suiv) != nullptr){    //doesnt work without ()
+        tmp = tmp->suiv;
+
+    }
+
+    tmp->suiv = toAdd;
+    
+    
+    
+
+}
 
 
 p_data ajoutDevant(DATATYPE uneval, p_data chain){
@@ -118,11 +138,51 @@ int nbCroissance(p_data chain){
 
 }
 
+void removeFirstEle(p_data & chain) { // remove first element of a chaine & return new head
+    
+    p_data exHead = chain; 
+    chain = chain->suiv;
+    free(exHead);
+
+    }
+
+p_data clone(p_data chain) { 
+
+    p_data tmp = (data*)malloc(sizeof(data)); ; 
+    tmp->valeur = chain->valeur; 
+    tmp->suiv = chain->suiv; 
+
+    return tmp;
+}
+
+//////////////TOFIX//////////////////
+void extraireCroissance(p_data & chain, p_data & mono) {
+
+    p_data tmp = clone(chain); // copy "valeur" and "suiv" of the head
+    ajoutFin((tmp->valeur),mono);  // first element of the chaine will always be added to mono
+    removeFirstEle(chain); // remove the head, second element in the chaine becomes the new head
+
+    while( ( (tmp->suiv) != nullptr) && ((tmp->valeur) < (chain->valeur) ) ) { // to extract the first monotonie
+
+        cout << (tmp->valeur) << " <  " << chain->valeur << endl;
+        
+        ajoutFin(chain->valeur,mono); //TOFIX
+        tmp = clone(chain); //TOFIX
+
+        removeFirstEle(chain);
+
+        (tmp->suiv) = nullptr;
+        
+    }
+
+
+}
+
 
 //------------------------------------------
 int main(){
    p_data head;
-   head = saisieNombre(10);
+   head = saisieNombre(3);
    affCh(head);
   
    int cpt = nbCroissance(head);
@@ -130,7 +190,15 @@ int main(){
    cout << endl << "nbmonotonies = " << cpt <<endl;
 
 
+//------test extraireCroissance(p_data & chain, p_data & mono) ----- 
 
+p_data mono = (data*)malloc(sizeof(data)); 
+mono->valeur="ah"; 
+mono->suiv=nullptr; 
 
+extraireCroissance(head,mono);
+affCh(mono);
+cout << "..." << endl;
+affCh(head);
 
 }
